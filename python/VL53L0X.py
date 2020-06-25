151a9796e599db8c37304b809432891c325a10de
Michael McKelvaney <michael@mckelvaney.co.uk>
2020-06-25 09:49:50 +0100

    Look up system suffix for loading shared object

--- python/VL53L0X.py
+++ python/VL53L0X.py
@@ -23,6 +23,12 @@
 # SOFTWARE.
 from ctypes import CDLL, CFUNCTYPE, POINTER, c_int, c_uint, pointer, c_ubyte, c_uint8, c_uint32
 import pkg_resources
+import sys, sysconfig
+
+suffix = sysconfig.get_config_var('EXT_SUFFIX')
+if suffix is None:
+    suffix = ".so"
+
 SMBUS='smbus'
 for dist in pkg_resources.working_set:
     #print(dist.project_name, dist.version)
@@ -78,15 +84,15 @@ _I2C_READ_FUNC = CFUNCTYPE(c_int, c_ubyte, c_ubyte, POINTER(c_ubyte), c_ubyte)
 _I2C_WRITE_FUNC = CFUNCTYPE(c_int, c_ubyte, c_ubyte, POINTER(c_ubyte), c_ubyte)
 
 # Load VL53L0X shared lib
-_POSSIBLE_LIBRARY_LOCATIONS = ['../bin'] + site.getsitepackages()
+_POSSIBLE_LIBRARY_LOCATIONS = ['../bin'] + sys.path
 for lib_location in _POSSIBLE_LIBRARY_LOCATIONS:
     try:
-        _TOF_LIBRARY = CDLL(lib_location + "/vl53l0x_python.so")
+        _TOF_LIBRARY = CDLL(lib_location + "/vl53l0x_python" + suffix)
         break
     except OSError:
         pass
 else:
-    raise OSError('Could not find vl53l0x_python.so')
+    raise OSError('Could not find vl53l0x_python' + suffix)
 
 
 class VL53L0X:
